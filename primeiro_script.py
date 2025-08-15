539e22982f25ce06876c224e74bc52efc5a9c333
FelipeDesda <Felipedamascenoesouza@gmail.com>
2025-08-15 09:21:57 -0300

    Adiciona primeiro scrip com dados pessoais

---
+++ primeiro_script.py
@@ -0,0 +1,12 @@
+soma = 0
+x=10
+y=20
+
+print("Minha soma é: ", x+y)
+
+nome = "Felipe"
+idade = 21
+Hobby = "Jogar video game"
+
+print("Olá, mundo!")
+print(f"Meu nome é {nome} e eu tenho {idade} anos, meu hobby é {Hobby}.")
